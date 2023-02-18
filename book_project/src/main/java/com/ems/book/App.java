f97d0bf9f3eddbdee8318ff16c746f255eda9972
Arpita-95 <124869571+Arpita-95@users.noreply.github.com>
2023-02-18 09:46:40 +0530

    Add files via upload

---
+++ book_project/src/main/java/com/ems/book/App.java
@@ -0,0 +1,15 @@
+package com.ems.book;
+import java.sql.Connection;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Connection conn=DataBaseConnectionCreation.getDBConnection();
+        System.out.println(conn);
+    }
+}
---
+++ book_project/src/main/java/com/ems/book/CreateTable.java
@@ -0,0 +1,22 @@
+package com.ems.book;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class CreateTable {
+	
+	public static void createTable()
+	{
+		try(Connection conn= DataBaseConnectionCreation.getDBConnection())
+		{
+			Statement st=conn.createStatement();
+			String sql="create table student_info(id int primary key, name varchar(30), email varchar(50),standard int)";
+			st.executeUpdate(sql);
+			System.out.println("table successfully created");
+			
+			
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
